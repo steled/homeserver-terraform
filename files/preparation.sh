09130e84e7df7d19a5f2bb3ffa362a01efb8f655
steled <steled@gmx.net>
2024-09-27 07:54:58 +0200

    update

--- files/preparation.sh
+++ files/preparation.sh
@@ -1,25 +1,59 @@
 #!/bin/bash
 
+# define tool versions
+# to check for versions use: apt-cache policy <PACKAGE>
+KUBERNETS_VERSION=1.30
+HELM_VERSION=3.15.1-1
+TERRAFROM_VERSION=1.4.6-1
+
+# install required packages
+sudo apt-get update
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y apt-transport-https ca-certificates curl gnupg software-properties-common
+
 # install kubectl
-sudo apt-get update && sudo apt-get install -y apt-transport-https
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmour -o /usr/share/keyrings/kubernetes.gpg
-echo "deb [arch=amd64 signed-by=/usr/share/keyrings/kubernetes.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
-sudo apt-get update
-sudo apt-get install -y kubectl=1.28.2-00
-sudo apt-mark hold kubectl
+## download Google Cloud public signing key
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v$KUBERNETS_VERSION/deb/Release.key | sudo gpg --batch --yes --dearmor -o /usr/share/keyrings/kubernetes.gpg
+# add Kubernetes apt repository
+echo "deb [signed-by=/usr/share/keyrings/kubernetes.gpg] https://pkgs.k8s.io/core:/stable:/v$KUBERNETS_VERSION/deb/ /" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
-# install helm
+## update apt package index and install kubectl
+sudo apt-get update
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y kubectl
+
+## add autocompletion
+kubectl completion bash | sudo tee /etc/bash_completion.d/kubectl > /dev/null
+
+# add k alias and autocompletion for alias
+echo 'alias k=kubectl' >> ~/.bash_aliases
+echo 'complete -o default -F __start_kubectl k' >> ~/.bashrc
+
+# install Helm
+## download baltocdn public signing key
 curl https://baltocdn.com/helm/signing.asc | gpg --dearmor | sudo tee /usr/share/keyrings/helm.gpg > /dev/null
-sudo apt-get install apt-transport-https --yes
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
-sudo apt-get update
-sudo apt-get install -y helm
 
-# install terraform
+## add Helm apt repository
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
+
+## update apt package index and install helm
+sudo apt-get update
+sudo DEBIAN_FRONTEND=noninteractive apt-get install helm=$HELM_VERSION
+
+## add autocompletion
+helm completion bash | sudo tee /etc/bash_completion.d/helm > /dev/null
+
+# install Terraform
+## download HashiCorp public signing key
 curl https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg > /dev/null
+
+## add HashiCorp apt repository
 echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
-sudo apt update
-sudo apt-get install -y terraform=1.6.4-1
+
+## update apt package index and install helm
+sudo apt-get update
+sudo DEBIAN_FRONTEND=noninteractive apt-get install terraform=$TERRAFROM_VERSION
+
+## add autocompletion
+terraform -install-autocomplete | sudo tee /etc/bash_completion.d/terraform > /dev/null
 
 # add kubectl completion
 sudo sh -c 'kubectl completion bash > /etc/bash_completion.d/kubectl'
