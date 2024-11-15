f6384a6d7371829b15b49f3390343899724178aa
steled <steled@gmx.net>
2024-11-15 07:58:26 +0100

    removed not used provider

--- providers.tf
+++ providers.tf
@@ -2,10 +2,10 @@ terraform {
   required_version = ">=1.3.0"
 
   required_providers {
-    ssh = {
-      source  = "loafoe/ssh"
-      version = "2.6.0"
-    }
+    # ssh = {
+    #   source  = "loafoe/ssh"
+    #   version = "2.6.0"
+    # }
 
     helm = {
       source  = "hashicorp/helm"
@@ -25,15 +25,15 @@ terraform {
     #   version = "0.12.0"
     # }
 
-    remote = {
-      source  = "tenstad/remote"
-      version = "0.1.2"
-    }
+    # remote = {
+    #   source  = "tenstad/remote"
+    #   version = "0.1.2"
+    # }
   }
 
   # backend "http" { }
 }
 
-provider "ssh" {}
+# provider "ssh" {}
 # provider "time" {}
-provider "remote" {}
+# provider "remote" {}
