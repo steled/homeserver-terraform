d4599c4bdae562c6baa0ef4d103d2588e222746e
steled <steled@gmx.net>
2024-11-15 12:51:20 +0100

    commiting decrypted files

--- nextcloud-stg.tf
+++ nextcloud-stg.tf
@@ -1,34 +1,29 @@
-U2FsdGVkX1+ZWtU4yc3h3ZzdPiga3edwGypwivIoXhNhzCfzUhxe3TbeoXwyahR+
-sD7eMdSQcJq2FBzAoVIYEmBs+JsARIwQ9e6817uCryJL9tx3UayOG6wKqAcT+zuf
-ftzsTOMxnVgery+kHzBjcXlPKyR2xAYi+2vVXM571CCls7o+T4r2ykHYL0xls4TC
-Z2NN0UymG4Ba2MI4YcM9hRA7XbDnluSro1TJ5gOf5zbPbJq7MBFCI56G4w0eI7nQ
-NDn1XSeuA/oFDpT7egkUaRQRqvua0540I5ssLq/WIMov5ms28NLELC67NYYi6GQt
-HJvI6eUZCtRthxQE50lbCHfTjsk7GxAcP6NSIUlsScQXbLr/t4bCRyj5c/Cx7as+
-Pv/4s7JTHUrr31O6dMLLB7LBBKw6xiXx6ztU9yzWMmkklUc5OL4vX8ow7nWy+1HA
-eRuiN0d8FPvPInKDMprebANuZEYhlQ33Mna2LCqBYj5nuaP2Cv50Tg8KOOzJHmqr
-3nZydkzBZHXVnRlDOcZcun/FqNdsytYTykRSuaQqAuLQwzdGzDo7N3W+UsxYtlXX
-ceSHmBukgGgmmzN8IjmMzmVfs0eY0JVHb0+pInXr95BEB8cMTq1FwrFrlriqSqKg
-3Id+HTjvlvMvz2PL3rSJRgqBUuUTSzAtDp74Z3w5qNoFeg8X2qM65s0l5n//Prf0
-kwJM4q1gbqUS4hlVmqkZEhpL49qBKqhiIrMu7qP0cK8pxFqElZf+vXetGc9DgbuU
-5pgm9pv+7yRL54zjG6Q+mKo47ia2LKe0bBEjKnUm3TQDmYjPO8TECLyMtRpPcEUw
-xH/eh8KYWqECAK2GyaW7xo+uybYkIgGQChOvTPkiRfJ3/rpzdu8kex1lIFlr6vqn
-laF7RsEq/hf9r/8z/s4ujr265Q9PLCPIEGWhI7ujOyrLwmv0amsxV62wbRT5M2O6
-K5y7dQz5Hu5U1ZcSTDjyODuvuEKDXvCfaNrMukcUMxSRs53h3Xo4YU4BirL7NlnZ
-Mlz0EStSEGmlh2gzs52Pxb75oEyrZMYugTXxl9+GdpiQ93qoJhtZ+h6Qpox+d/dE
-bswAVieJ0/87baqtUHO1sXoj7QprGF16IinMow+mJwgnO2KQb+hk9oAAaxT3BHP4
-UFhDzDktUv6VHgy58e+3GLAkR+sMDz+mQ0F+dUbEF0quNyA+eJCGifr46DqSfj/a
-HyQLtbMu38cTH8VKAdALJY5DrPbRlzJeenum3wmOFhE4JW64QVzCsx5Uj+bnPQAb
-NVHmw7rNrInX/9kZTua1gtkmO7G8ga7A88vg5UcJYjVKN0CCXdubpkbrsOMKW+yI
-+TZfFdQd21WavJ2Q1qdQW+hIi1VvWg8g6kqJ2HN6thTL53dn3DHxI+3YD6DvLyKG
-uSowxdLxM5zKkI3jjGfQWyYVGgJjYoLFwJxiXUY8PEwb0IyxQYQUXLOcZVeDBN8q
-RGsITKmQ14ZERz1p6/Z4fksUXkg1DGp9I9vaotngiffnoI9TVr8xVMkBe1IieANq
-pLlRbhEguDM2ZEWMYOxq5Tp8pmrGpYLoofYoKwKaEHWSHeDBQMIJnSeenj4yg/uo
-hXpRXNm+/5lKIUoheoR8hWBnhSLeCVsGbbbgWE3i9cuKd4+1UOGF283t4vC+SWoW
-t1YR8diw5aCBF1OSVIPSf1JsTGCAtFmewWvVqstgxWlWMy+EL7iUUyVksTPFME1U
-6NT4aenrfQIX5hL+ql31ul1QY7JSITQS2Bxogtt1evhNON8bNwEDefq+3zzAl+pz
-OxaEOxpiTTKXeNj171dXSb7+g2g0O96fTmFaGLasCh1kaEoffe5+Wb5k5n8hr1qq
-nhVc3Egcdicltfovu5SsMXO1TerT0QPF8gHZ3la1ncEU00xDyPCBqNuu1M3Gu/f3
-XpDABOGiD+Zic4gf1tJQJPsIJcRdEgO9+y0pSBqNdnyCkeTut7R9nPXDGS5J1tcP
-J2s+nLrt9sA0AY9ESRayy3wABnuETOngatnVvzpYhrKVWbNAfxjMngnq2PPMIwi0
-BikZ594stm43ZH+cb0ZCCxi9hIfjMm2/okJuZxFmsx1xjVVB/qUFbJoqtxRHU5vu
-hJNEhxBCoXBljb0jY7qaNEIcUyDcdwztzjNnePyTILakP/S3zIdt8G9g0lPSKevg
+module "nextcloud_staging" {
+  # source="git@github.com:steled/terraformmodules.git//nextcloud_staging?ref=modules"
+  source = "../terraformmodules/nextcloud_staging/"
+
+  ssh_user                  = var.server.user
+  ssh_host                  = var.server.host
+
+  nextcloud_version         = "6.2.1" # get version from here: https://github.com/nextcloud/helm/blob/e11836b1935a2a75022d93212687302697ef80ba/charts/nextcloud/Chart.yaml#L3
+  kubernetes_namespace_name = "nextcloud-staging"
+  maintenance_job_image     = "nextcloud:30.0.1-fpm" # get version from here: https://github.com/nextcloud/helm/blob/main/charts/nextcloud/Chart.yaml#L4
+  nextcloud_domain          = var.nextcloud_stg_domain
+  environment               = var.nextcloud_stg_env
+  ip_address                = var.nextcloud_stg_ip_address
+  nextcloud_admin_username  = var.nextcloud_admin_username
+  nextcloud_admin_password  = var.nextcloud_admin_password
+  nextcloud_proxies         = var.nextcloud_proxies
+  mail_fromaddress          = var.nextcloud_stg_mail_fromaddress
+  mail_domain               = var.nextcloud_mail_domain
+  smtp_host                 = var.nextcloud_smtp_host
+  smtp_port                 = "587"
+  smtp_username             = var.nextcloud_smtp_username
+  smtp_password             = var.nextcloud_smtp_password
+  redis_password            = var.nextcloud_redis_password
+  postgresql_username       = var.nextcloud_postgresql_username
+  postgresql_password       = var.nextcloud_postgresql_password
+  postgresql_database       = var.nextcloud_postgresql_database
+
+  depends_on = [ module.cert_manager_cloudflare ]
+}
