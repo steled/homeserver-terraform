ff334ca5dd64c19003588544fe2a38a9b31d42d4
steled <steled@gmx.net>
2024-11-15 06:37:50 +0100

    update

--- main.tf
+++ main.tf
@@ -1,22 +1,13 @@
 provider "kubernetes" {
-  config_path = "/home/steled/bm-cluster-kubeconfig"
-  # config_path = "/home/steled/.kube/172.31.70.5_config"
-  # config_path = "${local_sensitive_file.kubeconfig.filename}"
-  # config_path = "/home/steled/.kube/172.18.162.170_config"
-  # config_path = "${local_sensitive_file.kubeconfig[local.servers[0].host].filename}"
+  config_path = "/home/steled/.kube/config"
 }
 
 provider "helm" {
   kubernetes {
-    config_path = "/home/steled/bm-cluster-kubeconfig"
-    # config_path = "/home/steled/.kube/172.31.70.5_config"
-    # config_path = "${local_sensitive_file.kubeconfig.filename}"
-    # config_path = "${local_sensitive_file.kubeconfig[local.servers[0].host].filename}"
+    config_path = "/home/steled/.kube/config"
   }
 }
 
 provider "kubectl" {
-  config_path = "/home/steled/bm-cluster-kubeconfig"
-  # config_path = "${local_sensitive_file.kubeconfig.filename}"
-  # config_path = "/home/steled/.kube/172.31.70.5_config"
+  config_path = "/home/steled/.kube/config"
 }
