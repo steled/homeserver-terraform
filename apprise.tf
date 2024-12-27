756ce6ca25d2c6191b0ff64cfa9042a0b85a3876
steled <34886888+steled@users.noreply.github.com>
2024-12-27 15:19:29 +0000

    terraform fmt

--- apprise.tf
+++ apprise.tf
@@ -3,11 +3,11 @@ module "apprise" {
   source = "../terraformmodules/apprise/"
 
   # renovate: datasource=docker depName=apprise packageName=caronc/apprise
-  apprise_version              = "1.1.4" # check version here: https://registry.hub.docker.com/r/caronc/apprise/tags
-  kubernetes_namespace_name    = "apprise"
-  apprise_api_htpasswd_secret  = var.apprise_api_htpasswd_secret
-  environment                  = var.apprise_env_prd
-  domain                       = var.apprise_domain
+  apprise_version             = "1.1.4" # check version here: https://registry.hub.docker.com/r/caronc/apprise/tags
+  kubernetes_namespace_name   = "apprise"
+  apprise_api_htpasswd_secret = var.apprise_api_htpasswd_secret
+  environment                 = var.apprise_env_prd
+  domain                      = var.apprise_domain
 
   depends_on = [module.cert_manager_cloudflare]
 }
