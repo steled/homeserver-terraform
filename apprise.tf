08b7b84eb8d71d91d669022cec1ce1751e100e7a
steled <34886888+steled@users.noreply.github.com>
2024-12-27 15:37:17 +0000

    Update apprise Docker tag to v1.1.6

--- apprise.tf
+++ apprise.tf
@@ -3,7 +3,7 @@ module "apprise" {
   source = "../terraformmodules/apprise/"
 
   # renovate: datasource=docker depName=apprise packageName=caronc/apprise
-  apprise_version             = "1.1.4" # check version here: https://registry.hub.docker.com/r/caronc/apprise/tags
+  apprise_version             = "1.1.6" # check version here: https://registry.hub.docker.com/r/caronc/apprise/tags
   kubernetes_namespace_name   = "apprise"
   apprise_api_htpasswd_secret = var.apprise_api_htpasswd_secret
   environment                 = var.apprise_env_prd
