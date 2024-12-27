fb0d21492c0fddf24360d8e7ebc10e0568e95595
steled <steled@gmx.net>
2024-12-27 19:15:34 +0100

    fixing path

--- apprise.tf
+++ apprise.tf
@@ -1,5 +1,5 @@
 module "apprise" {
-  source = "git@github.com:steled/terraformmodules.git//authentik?ref=v0.7"
+  source = "git@github.com:steled/terraformmodules.git//apprise?ref=v0.7"
   # source = "../terraformmodules/apprise/"
 
   # renovate: datasource=docker depName=apprise packageName=caronc/apprise
