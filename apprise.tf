b04941a2bfc8f7fe484840c7523d7d2192fe61a8
steled <steled@gmx.net>
2025-01-31 15:06:47 +0100

    update

--- apprise.tf
+++ apprise.tf
@@ -2,10 +2,13 @@ module "apprise" {
   source = "git@github.com:steled/terraformmodules.git//apprise?ref=v0.14"
   # source = "../terraformmodules/apprise/"
 
+  ssh_user = var.server.user
+  ssh_host = var.server.host
+
   # renovate: datasource=docker depName=apprise packageName=caronc/apprise
   apprise_version             = "1.1.8" # check version here: https://registry.hub.docker.com/r/caronc/apprise/tags
   kubernetes_namespace_name   = "apprise"
-  apprise_api_htpasswd_secret = var.apprise_api_htpasswd_secret
+  # apprise_api_htpasswd_secret = var.apprise_api_htpasswd_secret
   environment                 = var.apprise_env_prd
   domain                      = var.apprise_domain
 
