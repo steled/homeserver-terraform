b5b9f974fce6d7953a652b906ee88e2f2cba950f
steled <steled@gmx.net>
2025-02-16 11:23:58 +0100

    cleanup

--- providers.tf
+++ providers.tf
@@ -2,11 +2,6 @@ terraform {
   required_version = "~> 1.10"
 
   required_providers {
-    # ssh = {
-    #   source  = "loafoe/ssh"
-    #   version = "2.6.0"
-    # }
-
     helm = {
       source  = "hashicorp/helm"
       version = "~> 2.17"
@@ -19,21 +14,5 @@ terraform {
       source  = "alekc/kubectl"
       version = "~> 2.1"
     }
-
-    # time = {
-    #   source  = "hashicorp/time"
-    #   version = "0.12.0"
-    # }
-
-    # remote = {
-    #   source  = "tenstad/remote"
-    #   version = "0.1.2"
-    # }
   }
-
-  # backend "http" { }
 }
-
-# provider "ssh" {}
-# provider "time" {}
-# provider "remote" {}
