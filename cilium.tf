96bf07502eee9a23567d59e7cf43e2fae64030a3
steled <steled@gmx.net>
2025-02-16 11:19:35 +0100

    cleanup

--- cilium.tf
+++ cilium.tf
@@ -1,9 +1,3 @@
-# resource "time_sleep" "wait_30_seconds" {
-#   create_duration = "30s"
-
-#   depends_on = [ module.cilium ]
-# }
-
 resource "kubectl_manifest" "ciliumloadbalancerippool" {
   yaml_body = <<YAML
 apiVersion: cilium.io/v2alpha1
@@ -15,8 +9,6 @@ spec:
   blocks:
   - cidr: ${var.cilium_cidr}
 YAML
-
-  # depends_on = [ time_sleep.wait_30_seconds ]
 }
 
 resource "kubectl_manifest" "ciliuml2announcementpolicy" {
